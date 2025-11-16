b9803108d3569e3a399a95e776e14fd777d6d4d4
Eric <273393@stud.inn.no>
2025-11-16 17:42:32 +0100

    Fix Firebase Storage bucket URL format
    Changed storageBucket from snop-b76ac.firebasestorage.app to snop-b76ac.appspot.com to match Firebase's default bucket format. This should resolve the storage/unknown error when uploading audio files.

--- snop/mobile/src/services/firebase.js
+++ snop/mobile/src/services/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAKF663iLJDW4p5luNm0_avaS0Apeo-5Ow",
   authDomain: "snop-b76ac.firebaseapp.com",
   projectId: "snop-b76ac",
-  storageBucket: "snop-b76ac.firebasestorage.app",
+  storageBucket: "snop-b76ac.appspot.com",
   messagingSenderId: "211954707057",
   appId: "1:211954707057:web:a52a9afc2133aeb60789d0",
   measurementId: "G-WW0ZQGB68L"
